39b12868ad1cfb18dc0edf07727173215bddb7c9
Rodrigosik <Rodrigosik@users.noreply.github.com>
2020-05-15 18:10:21 -0500

    Agregando al asider los proveedores

--- vistas/modulos/sidebar.php
+++ vistas/modulos/sidebar.php
@@ -6,7 +6,7 @@
     alt="AdminLTE Logo"
     class="brand-image img-circle elevation-3"
     style="opacity: .8">
-    <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <span class="brand-text font-weight-light">PerseoV1.0</span>
   </a>
 
   <!-- Sidebar -->
@@ -17,7 +17,7 @@
         <img src="vistas/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="#" class="d-block">Perseo el Semi Dios</a>
       </div>
     </div>
 
@@ -66,6 +66,26 @@
             </ul>
           </li>
           <!-- Finaliza modulo de clientes -->
+
+           <!-- Inicia modulo de Proveedores -->
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="far fa-address-card nav-icon"></i>
+              <p>
+                Proveedores
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="proveedores" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Lista de Proveedores</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!-- Finaliza modulo de Proveedores -->
 
         </ul>
       </nav>
