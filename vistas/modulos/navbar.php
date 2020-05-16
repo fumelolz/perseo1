d6f224129aeafa91de9913c0a4f851459746c6d8
fumelolz <thefumetero@gmail.com>
2020-05-15 23:31:52 -0500

    Su pedo del rodrigo, yo no entender
    Su pedo

--- vistas/modulos/navbar.php
+++ vistas/modulos/navbar.php
@@ -1,6 +1,6 @@
   
 <!-- Navbar -->
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+<nav class="main-header navbar navbar-expand navbar-personalizado navbar-dark">
   <!-- Left navbar links -->
   <ul class="navbar-nav">
     <li class="nav-item">
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -18,6 +18,8 @@
   <link rel="stylesheet" href="vistas/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
   <link rel="stylesheet" href="vistas/plugins/datatables-responsive/css/responsive.bootstrap4.css">
 
+  <link rel="stylesheet" href="vistas/css/estilos.css">
+
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
