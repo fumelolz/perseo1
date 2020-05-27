1416b9181d3cd12d4ab6f5c2dee6657c1c2c765d
fumelolz <thefumetero@gmail.com>
2020-05-27 12:22:22 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -3,7 +3,7 @@
 class Conexion {
 
 	static public function conectar(){
-		$link = new PDO("mysql:host=187.227.134.204;dbname=perseov1","perseo","perseo123");
+		$link = new PDO("mysql:host=187.227.128.234;dbname=perseov1","perseo","perseo123");
 		$link->exec("set names utf8");
 		return $link;
 	}
