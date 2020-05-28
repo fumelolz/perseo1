8495f000e0daa4ed108855153d368a67ea923261
fumelolz <thefumetero@gmail.com>
2020-05-28 15:45:18 -0500

    Cambiado la forma de presentar al usuario

--- vistas/modulos/sidebar.php
+++ vistas/modulos/sidebar.php
@@ -12,14 +12,14 @@
   <!-- Sidebar -->
   <div class="sidebar">
     <!-- Sidebar user (optional) -->
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+    <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
         <img src="vistas/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
         <a href="#" class="d-block color-icono">Perseo el Semi Dios</a>
       </div>
-    </div>
+    </div> -->
 
     <!-- Sidebar Menu -->
     <nav class="mt-2">
